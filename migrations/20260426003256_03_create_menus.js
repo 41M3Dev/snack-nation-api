2a750410c05c71dec059a98e17f61c6ba2b31b73
41M3Dev <102484378+41M3Dev@users.noreply.github.com>
2026-04-26 23:02:30 +0200

    chore: ajout migration menus

--- migrations/20260426003256_03_create_menus.js
+++ migrations/20260426003256_03_create_menus.js
@@ -1,15 +1,22 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
+ * @returns {Knex.SchemaBuilder}
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('menus', (table) => {
+        table.increments('id').primary();
+        table.string('name', 150).notNullable();
+        table.decimal('price_base', 10, 2).notNullable();
+        table.tinyint('is_active').notNullable().defaultTo(1);
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
+ * @returns {Knex.SchemaBuilder}
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('menus');
 };
