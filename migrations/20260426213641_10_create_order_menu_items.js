6ceaf9e2e045efa0943aab7e48841cb69e80ffbe
41M3Dev <102484378+41M3Dev@users.noreply.github.com>
2026-04-26 23:47:19 +0200

    chore: ajout migration order_menu_items

--- migrations/20260426213641_10_create_order_menu_items.js
+++ migrations/20260426213641_10_create_order_menu_items.js
@@ -23,5 +23,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schema.dropTable("order_lines");
+    return knex.schema.dropTable("order_menu_items");
 };
