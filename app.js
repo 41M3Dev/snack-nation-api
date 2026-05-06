6c0d055d9518de9c8badcd71958b47e8eacdc05e
41M3Dev <102484378+41M3Dev@users.noreply.github.com>
2026-05-06 14:02:36 +0200

    feat: authentification JWT login et middlewares auth/authorize

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
 const app = express();
+app.use(express.json());
+const authRoutes = require('./src/routes/auth');
+app.use('/api/auth', authRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
